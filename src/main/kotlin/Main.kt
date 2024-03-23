97ceb6c0ed89fc338c899c49569be540c11a6d1e
Miguel Valadares <mlvss@iscte-iul.pt>
2024-03-23 11:38:59 +0000

    estrutura base

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,20 @@
-class Document{
-    internal val children = mutableListOf<Tag>()
+class Document(
+    val rootTag: Tag
+)
+
+interface Tag {
+    val name: String
+    val attributes: Map<String, String>
 }
 
-class Tag{
+data class CompositeTag(
+    override val name: String,
+    override val attributes: Map<String, String>  = emptyMap(),
+    val children: List<Tag> = emptyList()
+) : Tag
 
-}
-
+data class StringTag(
+    override val name: String,
+    override val attributes: Map<String, String> = emptyMap(),
+    val content : String //caso não tenha nada, é CompositeTag
+) : Tag
