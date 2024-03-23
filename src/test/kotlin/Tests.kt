2c21b9d03db4f9792b8ed3dc2027a399316982b2
Miguel Valadares <mlvss@iscte-iul.pt>
2024-03-23 12:05:49 +0000

    alguns testes

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -6,8 +6,33 @@ import org.junit.jupiter.api.Test
 //org.junit.jupiter:junit-jupiter:5.10.2
 class Tests {
 
+    val document = Document(
+        CompositeTag(
+            "plano",
+            children = listOf(
+                StringTag(
+                    "Mestrado em Engenharia Informática",
+                    content = "olá"
+                )
+            )
+        )
+    )
+
     @Test
-    fun test(){
+    fun test0(){
         assertTrue(true)
+    }
+
+    @Test
+    fun test1(){
+        assertEquals("plano", document.rootTag.name)
+        assertEquals(1, (document.rootTag as CompositeTag).children.size)
+    }
+
+    @Test
+    fun test2(){
+        val childTag = (document.rootTag as CompositeTag).children[0] as StringTag
+        assertEquals("Mestrado em Engenharia Informática", childTag.name)
+        assertEquals("olá", childTag.content)
     }
 }
